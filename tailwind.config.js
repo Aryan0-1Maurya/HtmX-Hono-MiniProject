b30e2efb35549c76705d5e027ab4bfdb2c4db683
sharklightning <careythomas91@gmail.com>
2024-05-25 01:11:41 -0500

    added safelist to tailwind config to prevent it from removing my table row dropdown styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,10 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms'),
   ],
+  safelist: [
+    'flex', 'items-center', 'rounded-full', 'bg-gray-100', 'text-gray-400', 'hover:text-gray-600', 
+    'focus:outline-none', 'focus:ring-2', 'focus:ring-indigo-500', 'focus:ring-offset-2', 
+    'focus:ring-offset-gray-100', 'h-5', 'w-5', 'sr-only'
+  ],
 }
 
